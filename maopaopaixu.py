6187eff73f6721f1c8ceb985fcac20856a92531c
yexiaozhu <557703988@139.com>
2018-03-20 10:55:38 +0800

    冒泡排序

---
+++ maopaopaixu.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: UTF-8  -*-
+# author: yexiaozhu
+
+
+def bubble(list):
+    list_len = len(list)
+    while list_len > 0:
+        for j in range(list_len - 1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+            print('j:', j)
+            print('list=', list)
+        list_len -= 1
+    print('list=', list)
+list = [1, 25, 15, 7, 10, 5]
+#list = [1, 25]
+bubble(list)
